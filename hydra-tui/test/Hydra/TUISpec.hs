add22ea15b94c8a4544bcb56475958025c660dfd
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-10-19 18:40:20 +0200

    Revert change to unix library

--- hydra-tui/test/Hydra/TUISpec.hs
+++ hydra-tui/test/Hydra/TUISpec.hs
@@ -241,7 +241,7 @@ withTUITest region action = do
     as <- newIORef initialAssumedState
     -- NOTE(SN): The null device should allow using this in CI, while we do
     -- capture the output via `outputByteBuffer` anyway.
-    nullFd <- openFd "/dev/null" WriteOnly defaultFileFlags
+    nullFd <- openFd "/dev/null" WriteOnly Nothing defaultFileFlags
     realOut <- outputForConfig $ defaultConfig{outputFd = Just nullFd, termName = Just "xterm"}
     closeFd nullFd
     let output = testOut realOut as frameBuffer
